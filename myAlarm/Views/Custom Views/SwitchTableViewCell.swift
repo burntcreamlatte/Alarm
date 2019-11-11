5696538d5be9f5d1e2ca1014d637dc65091f7b59
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-11-11 14:42:43 -0700

    storyboard elements placed, outlets/actions created

--- myAlarm/Views/Custom Views/SwitchTableViewCell.swift
+++ myAlarm/Views/Custom Views/SwitchTableViewCell.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class SwitchTableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    
+    // MARK: - Actions
+    @IBAction func switchValueChanged(_ sender: Any) {
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
